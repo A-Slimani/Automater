208bafb515446fb4ebc9a05188d45c354eae0f12
A-Slimani <abdullahslimani98@gmail.com>
2023-04-30 21:04:28 +1000

    Add project files.

---
+++ Automater/Program.cs
@@ -0,0 +1,25 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Edge;
+using Automater;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        IWebDriver driver = new EdgeDriver();
+        driver.Navigate().GoToUrl("https://www.bing.com/");
+        ICollection<IWebElement> elements = driver.FindElements(By.XPath("//*[@id]"));
+
+        IEnumerable<IWebElement> clickableElements = SeleniumFunctions.FindAllClickableElements(elements);
+
+        foreach (IWebElement element in clickableElements)
+        {
+            if (element.Text.Equals("Rewards"))
+            {
+                Console.WriteLine("Rewards Found");
+            }
+        }
+
+        driver.Quit();
+    }
+}
---
+++ Automater/SeleniumFunctions.cs
@@ -0,0 +1,28 @@
+﻿using OpenQA.Selenium;
+
+namespace Automater
+{
+    internal class SeleniumFunctions
+    {
+        public static IEnumerable<IWebElement> FindAllClickableElements(ICollection<IWebElement> webElements)
+        {
+
+            foreach (IWebElement element in webElements)
+            {
+
+                bool isClickable = false;
+
+                try
+                {
+                    isClickable = element.Enabled && element.Displayed;
+                }
+                catch (StaleElementReferenceException ex)
+                {
+                    Console.WriteLine($"Error {ex.Message}");
+                }
+
+                if (isClickable) yield return element;
+            }
+        }
+    }
+}
