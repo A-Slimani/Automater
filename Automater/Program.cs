c12cdabf9dc0a267c7e299038d4044d6a174239d
AbdullahS98 <44589099+AbdullahS98@users.noreply.github.com>
2023-05-05 12:37:02 +1000

    udpates

--- Automater/Program.cs
+++ Automater/Program.cs
@@ -7,37 +7,37 @@ using System.Text.RegularExpressions;
 
 class Program
 {
-    static void Main(string[] args)
-    {
-        EdgeOptions options = new EdgeOptions();
-        options.AddExcludedArgument("enable-logging");
-        IWebDriver driver = new EdgeDriver(options);
+  static void Main(string[] args)
+  {
+    EdgeOptions options = new EdgeOptions();
+    options.AddExcludedArgument("enable-logging");
+    IWebDriver driver = new EdgeDriver(options);
 
-        driver.Navigate().GoToUrl("https://rewards.bing.com/?signin=1");
+    driver.Navigate().GoToUrl("https://rewards.bing.com/?signin=1");
 
-        ICollection<IWebElement> cardElements =
-            driver.FindElements(By.XPath("//mee-rewards-daily-set-item-content | //mee-rewards-more-activities-card-item"));
+    ICollection<IWebElement> cardElements =
+        driver.FindElements(By.XPath("//mee-rewards-daily-set-item-content | //mee-rewards-more-activities-card-item"));
 
-        // Dont think I will need this anymore
-        // ICollection<IWebElement> pointElements = driver.FindElements(By.XPath("//*[@class='mee-icon mee-icon-AddMedium']"));
+    // Dont think I will need this anymore
+    // ICollection<IWebElement> pointElements = driver.FindElements(By.XPath("//*[@class='mee-icon mee-icon-AddMedium']"));
 
-        SeleniumFunctions.downloadAllElements(cardElements, "card-elements", e => e.GetAttribute("innerHTML"), "html");
+    // SeleniumFunctions.downloadAllElements(cardElements, "card-elements", e => e.GetAttribute("innerHTML"), "html");
 
-        ICollection<IWebElement> filteredElements =
-            SeleniumFunctions.FilterElements(cardElements, new Regex(@"mee-icon-AddMedium")).ToList();
+    ICollection<IWebElement> filteredElements =
+        SeleniumFunctions.FilterElements(cardElements, new Regex(@"mee-icon-AddMedium")).ToList();
 
-        SeleniumFunctions.downloadAllElements(filteredElements, "filtered_elements", e => e.Text, "txt");
+    // REACTIVATE THIS 
+    // SeleniumFunctions.downloadAllElements(filteredElements, "filtered_elements", e => e.Text, "txt");
 
-        Actions action = new Actions(driver);
-        foreach (IWebElement element in filteredElements)
-        {
-            action.KeyDown(Keys.Control).Click(element).KeyUp(Keys.Control).Build().Perform();
-        }
+    // REACTIVATE THIS
+    // SeleniumFunctions.ActivateRewards(filteredElements, driver);
 
-        // END CLOSING E
-        // 1000 = 1 sec
-        // Console.WriteLine("program will end in 30 seconds...");
-        // Thread.Sleep(1000 * 30);
-        // driver.Quit();
-    }
+    SeleniumFunctions.AutomatedSearches(driver);
+
+    // END APPLICATION 
+    int seconds = 15;
+    Console.WriteLine($"program will end in {seconds} seconds...");
+    Thread.Sleep(1000 * seconds);
+    driver.Quit();
+  }
 }
--- Automater/SeleniumFunctions.cs
+++ Automater/SeleniumFunctions.cs
@@ -1,102 +1,118 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Edge;
 using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.UI;
 using System.Text.RegularExpressions;
 
 namespace Automater
 {
-    internal class SeleniumFunctions
+  internal class SeleniumFunctions
+  {
+    public static IWebDriver currentBrowserInstance()
     {
-        public static IWebDriver currentBrowserInstance()
-        {
-            EdgeOptions options = new EdgeOptions();
-            options.DebuggerAddress = "localhost:9222";
+      EdgeOptions options = new EdgeOptions();
+      options.DebuggerAddress = "localhost:9222";
 
-            return new EdgeDriver(options);
-        }
-
-        public static void downloadAllElements(ICollection<IWebElement> elements, string filename, Func<IWebElement, string> getInfo, string fileType)
-        {
-            string filepath = @$"D:\programming\C#\Automater\Automater\{filename}.{fileType}";
-
-            if (File.Exists(filepath)) File.Delete(filepath);
-
-            string result = $"{elements.Count} results found for {filename}";
-            Console.WriteLine(result);
-
-            foreach (IWebElement element in elements)
-            {
-                if (!string.IsNullOrEmpty(getInfo(element)))
-                {
-                    File.AppendAllText(filepath, getInfo(element));
-                    File.AppendAllText(filepath, "\n");
-                }
-            }
-        }
-
-        // learn about classes - try chatgpt to improve these functions i.e. Polymorphism
-        // Write both functions first
-        public static IEnumerable<IWebElement> GetAllClickableElements(ICollection<IWebElement> webElements)
-        {
-
-            foreach (IWebElement element in webElements)
-            {
-
-                bool isClickable = false;
-
-                try
-                {
-                    isClickable = element.Enabled && element.Displayed;
-                }
-                catch (StaleElementReferenceException ex)
-                {
-                    Console.WriteLine($"Error {ex.Message}");
-                }
-
-                if (isClickable) yield return element;
-            }
-        }
-
-        public static IEnumerable<IWebElement> FilterElements(ICollection<IWebElement> webElements, Regex regex)
-        {
-            foreach (IWebElement element in webElements)
-            {
-                if (regex.IsMatch(element.GetAttribute("innerHTML")))
-                {
-                    yield return element;
-                }
-            }
-        }
-
-        public static void ActivateRewards(ICollection<IWebElement> webElements, IWebDriver driver)
-        {
-            Actions actions = new Actions(driver);
-            foreach (IWebElement element in webElements)
-            {
-                // this only covers rewards that dont have another complete scenario
-
-                // make this only get one tab at a time
-                actions.KeyDown(Keys.Control).Click(element).KeyUp(Keys.Control).Build().Perform();
-
-                // this needs to be done 
-                /*
-                WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
-                wait.Until(driver =>
-                {
-                    if (((IJavaScriptExecutor)driver).ExecuteScript("return document.readyState").Equals("complete"))
-                    {
-                        Console.WriteLine("Page loaded.");
-                        return true;
-                    }
-                    else
-                    {
-                        Console.WriteLine("Failed to load.");
-                        return false;
-                    }
-                });
-                */
-                // have another check once the page is open below
-            }
-        }
+      return new EdgeDriver(options);
     }
+
+    public static void downloadAllElements(ICollection<IWebElement> elements, string filename, Func<IWebElement, string> getInfo, string fileType)
+    {
+      string filepath = @$"D:\programming\C#\Automater\Automater\{filename}.{fileType}";
+
+      if (File.Exists(filepath)) File.Delete(filepath);
+
+      string result = $"{elements.Count} results found for {filename}";
+      Console.WriteLine(result);
+
+      foreach (IWebElement element in elements)
+      {
+        if (!string.IsNullOrEmpty(getInfo(element)))
+        {
+          File.AppendAllText(filepath, getInfo(element));
+          File.AppendAllText(filepath, "\n");
+        }
+      }
+    }
+
+    // learn about classes - try chatgpt to improve these functions i.e. Polymorphism
+    // Write both functions first
+    public static IEnumerable<IWebElement> GetAllClickableElements(ICollection<IWebElement> webElements)
+    {
+
+      foreach (IWebElement element in webElements)
+      {
+
+        bool isClickable = false;
+
+        try
+        {
+          isClickable = element.Enabled && element.Displayed;
+        }
+        catch (StaleElementReferenceException ex)
+        {
+          Console.WriteLine($"Error {ex.Message}");
+        }
+
+        if (isClickable) yield return element;
+      }
+    }
+
+    public static IEnumerable<IWebElement> FilterElements(ICollection<IWebElement> webElements, Regex regex)
+    {
+      foreach (IWebElement element in webElements)
+      {
+        if (regex.IsMatch(element.GetAttribute("innerHTML")))
+        {
+          yield return element;
+        }
+      }
+    }
+
+    public static void ActivateRewards(ICollection<IWebElement> webElements, IWebDriver driver)
+    {
+      Actions actions = new Actions(driver);
+      foreach (IWebElement element in webElements)
+      {
+        // this only covers rewards that dont have another complete scenario
+
+        // make this only get one tab at a time then closes it
+        actions.KeyDown(Keys.Control).Click(element).KeyUp(Keys.Control).Build().Perform();
+
+        WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+        wait.Until(driver =>
+        {
+          if (((IJavaScriptExecutor)driver).ExecuteScript("return document.readyState").Equals("complete"))
+          {
+            Console.WriteLine("Page loaded.");
+            // create a step to close the tab once its loaded here
+            return true;
+          }
+          else
+          {
+            Console.WriteLine("Failed to load.");
+            return false;
+          }
+        });
+        // have another check once the page is open below to complete pages with questions
+        // if (has some question element ....) 
+      }
+    }
+
+    public static void AutomatedSearches(IWebDriver driver)
+    {
+      driver.Navigate().GoToUrl("https://bing.com");
+
+      IWebElement searchBar = driver.FindElement(By.XPath("//*[@id=\"sb_form_q\"]"));
+      searchBar.SendKeys($"{}")
+      
+      Actions actions = new Actions(driver); 
+
+      
+      // get a random word and search it
+
+
+
+    }
+  }
 }
