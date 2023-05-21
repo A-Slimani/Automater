7f7089db5de9477a36d0f5390ce43da045f65da1
A-Slimani <abdullahslimani98@gmail.com>
2023-05-21 13:42:18 +1000

    refractoring

--- Automater/BingFunctions.cs
+++ Automater/BingFunctions.cs
@@ -1,162 +1,132 @@
-﻿using OpenQA.Selenium;
-using OpenQA.Selenium.Edge;
+﻿using Automater;
+using OpenQA.Selenium;
 using OpenQA.Selenium.Interactions;
 using OpenQA.Selenium.Support.Extensions;
 using OpenQA.Selenium.Support.UI;
 using SeleniumExtras.WaitHelpers;
+
 using System.Text.RegularExpressions;
 
-namespace Automater
+public class BingFunctions
 {
-    internal static class BingFunctions
-    {
-        public static IWebDriver CurrentBrowserInstance()
-        {
-            var options = new EdgeOptions
-            {
-                DebuggerAddress = "localhost:9222"
-            };
-            return new EdgeDriver(options);
-        }
+	private const string BingUrl = "https://bing.com";
+	private const string RewardsUrl = "https://rewards.bing.com/?signin=1";
+	private static readonly string WordListFilePath = Path.Combine(Directory.GetCurrentDirectory(), "word_list.txt");
 
-        public static IWebDriver InitialiseEdgeDriver()
-        {
-            EdgeOptions options = new();
-            options.AddExcludedArgument("enable-logging");
-            return new EdgeDriver(options);
-        }
+	private readonly IWebDriver _driver;
 
-        // Create a base class then make it a lil different depending on the card??
-        public static void ActivateRewardCards(IWebDriver driver)
-        {
-            driver.Navigate().GoToUrl("https://rewards.bing.com/?signin=1");
+	public BingFunctions(IWebDriver driver)
+	{
+		_driver = driver;
+	}
 
-            // maybe add a check here if it ever breaks
-            var cardElements = driver.FindElements(By.XPath("//mee-rewards-daily-set-item-content | //mee-rewards-more-activities-card-item"));
+	public void ActivateRewardCards()
+	{
+		_driver.Navigate().GoToUrl(RewardsUrl);
 
-            var filteredElements = BingElements.FilterElements(cardElements, new Regex(@"mee-icon-AddMedium")).ToList();
+		var cardElements = _driver.FindElements(By.XPath("//mee-rewards-daily-set-item-content | //mee-rewards-more-activities-card-item"));
+		var filteredElements = BingElements.FilterElements(cardElements, new Regex(@"mee-icon-AddMedium")).ToList();
 
-            var actions = new Actions(driver);
-            foreach (var element in filteredElements)
-            {
-                actions.KeyDown(Keys.Control).Click(element).KeyUp(Keys.Control).Build().Perform();
+		var actions = new Actions(_driver);
+		foreach (var element in filteredElements)
+		{
+			actions.KeyDown(Keys.Control).Click(element).KeyUp(Keys.Control).Build().Perform();
 
-                // filters out other text from the element
-                string cardNameText = element.Text.Split(new string[] { "\r\n" }, StringSplitOptions.None)[1];
+			string cardNameText = element.Text.Split(new string[] { "\r\n" }, StringSplitOptions.None)[1];
+			var questionCard = new Regex("(quiz|question|poll)", RegexOptions.IgnoreCase);
+			if (questionCard.IsMatch(cardNameText))
+			{
+				// ActivateCardQuestionsAndPolls(_driver, _driver.Url);
+				Console.WriteLine($"--- QUESTION CARD --- {cardNameText} --- FOUND");
+			}
 
-                // add a regex check if `cardNameText` equals to either quiz, question or poll
-                Regex questionCard = new Regex("(quiz|question|poll)", RegexOptions.IgnoreCase);
+			var answersText = _driver.FindElements(By.ClassName("bt_cardText"));
+			answersText.Select(answers => answers.Text).ToList().ForEach(Console.WriteLine);
 
-                if (questionCard.IsMatch(cardNameText))
-                {
-                    // run ActivateCardQuestionsAndPolls() here
-                    Console.WriteLine($"--- QUESTION CARD --- {cardNameText} --- FOUND");
-                }
+			var wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(10));
+			wait.Until(dv =>
+			{
+				var scriptResult = ((IJavaScriptExecutor)dv).ExecuteScript("return document.readyState");
+				if (scriptResult.Equals("complete"))
+				{
+					Console.WriteLine($"Reward card {cardNameText} complete.");
+					dv.SwitchTo().Window(dv.WindowHandles.Last()).Close();
+					dv.SwitchTo().Window(dv.WindowHandles.First());
+					return true;
+				}
+				else
+				{
+					Console.WriteLine($"Reward card {cardNameText} failed to load.");
+					return false;
+				}
+			});
+		}
+	}
 
-                var answersText = driver.FindElements(By.ClassName("bt_cardText"));
-                answersText.Select(answers => answers.Text).ToList().ForEach(Console.WriteLine);
+	public void ActivateCardQuestionsAndPolls(string url)
+	{
+		_driver.Navigate().GoToUrl(url);
 
-                var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
-                wait.Until(dv =>
-                {
-                    if (((IJavaScriptExecutor)dv).ExecuteScript("return document.readyState").Equals("complete"))
-                    {
-                        Console.WriteLine($"Reward card {cardNameText} complete.");
-                        dv.SwitchTo().Window(dv.WindowHandles.Last()).Close();
-                        dv.SwitchTo().Window(dv.WindowHandles.First());
-                        return true;
-                    }
-                    else
-                    {
-                        Console.WriteLine($"Reward card {cardNameText} failed to load.");
-                        return false;
-                    }
-                });
-            }
-        }
+		var wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(10));
+		var startQuizElement = wait.Until(ExpectedConditions.ElementToBeClickable(By.Id("rqStartQuiz")));
+		_driver.ExecuteJavaScript("arguments[0].click()", startQuizElement);
 
-        public static void ActivateCardQuestionsAndPolls(IWebDriver driver, string url)
-        {
-            // find the quiz element on the page  
-            // have some sort of flow chart to click on it
+		wait.Until(ExpectedConditions.ElementIsVisible(By.ClassName("btOptions")));
 
-            // list of potential buttons
-            // id = rqStartQuiz
-            //  -> class btOptions
-            //  -> 
-            // class = btOptions2 bt_pollOptions
+		var answerElements = _driver.FindElements(By.ClassName("bt_cardText"));
+		foreach (var element in answerElements)
+		{
+			if (element.GetCssValue("color") != "rbga(200, 0, 0, 1)")
+			{
+				_driver.ExecuteJavaScript("arguments[0].click()", element);
+			}
+		}
+	}
 
-            // just a test url for now
-            driver.Navigate().GoToUrl(url);
-            // driver.Navigate().GoToUrl(url);
+	public void AutomatedSearches()
+	{
+		_driver.Navigate().GoToUrl(BingUrl);
 
-            var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+		var lines = File.ReadAllLines(WordListFilePath);
+		var wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(10));
 
-            var startQuizElement = wait.Until(ExpectedConditions.ElementToBeClickable(By.Id("rqStartQuiz")));
+		int remainingPoints = BingElements.GetRemainingPoints(_driver);
+		while (remainingPoints > 0)
+		{
+			var randomWord = lines[new Random().Next(lines.Length)];
 
-            // hack since the standard click doesnt work. For some reason there another element ontop of it 
-            // preventing it from being clicked
-            driver.ExecuteJavaScript("arguments[0].click()", startQuizElement);
+			Console.WriteLine($"Searching for {randomWord}");
 
-            wait.Until(ExpectedConditions.ElementIsVisible(By.ClassName("btOptions")));
-            // make it an if statement check if its already been clicked. by the tick or cross
-            // maybe use a find first of that element 
-            var answerElements = driver.FindElements(By.ClassName("bt_cardText"));
+			try
+			{
+				var searchBar = wait.Until(ExpectedConditions.ElementToBeClickable(By.Id("sb_form_q")));
+				searchBar.Clear();
+				searchBar.SendKeys(randomWord);
+				searchBar.Submit();
+				wait.Until(ExpectedConditions.TitleContains(randomWord));
+				remainingPoints--;
 
-            foreach (var element in answerElements)
-            {
-                if (element.GetCssValue("color") != "rbga(200, 0, 0, 1)")
-                {
-                    driver.ExecuteJavaScript("arguments[0].click()", element);
-                }
-                // answerElements = wait.Until(ExpectedConditions.ElementIsVisible(By.ClassName("bt_cardText")));
-            }
-        }
+				Console.WriteLine($"Search complete. {remainingPoints} searches remaining");
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine($"Search failed: {ex}");
+			}
 
-        public static void AutomatedSearches(IWebDriver driver)
-        {
-            driver.Navigate().GoToUrl("https://bing.com");
+			if (remainingPoints == 0) remainingPoints = BingElements.GetRemainingPoints(_driver);
+		}
+	}
 
-            var lines = File.ReadAllLines(Path.Combine(Directory.GetCurrentDirectory(), "word_list.txt"));
+	public void ActivateQuestAndPunchCards()
+	{
+		// TODO: Implement this method
+		// function will be called after the card search
+	}
 
-            var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
-
-
-            int remainingPoints = BingElements.GetRemainingPoints(driver);
-            while (remainingPoints > 0)
-            {
-                driver.Navigate().GoToUrl("https://bing.com");
-
-                var randomWord = lines[new Random().Next(lines.Length)];
-
-                Console.WriteLine($"Searching for {randomWord}");
-
-                try
-                {
-                    var searchBar = wait.Until(ExpectedConditions.ElementToBeClickable(By.Id("sb_form_q")));
-                    searchBar.Clear();
-                    searchBar.SendKeys(randomWord);
-                    searchBar.Submit();
-                    wait.Until(ExpectedConditions.TitleContains(randomWord));
-                    remainingPoints--;
-
-                    Console.WriteLine($"Search complete. {remainingPoints} searches remaining");
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine($"Search failed: {ex}");
-                }
-
-                // make sure to go back and check if all points are caught
-                if (remainingPoints == 0) remainingPoints = BingElements.GetRemainingPoints(driver);
-            }
-        }
-
-        public static void CloseSelenium(int seconds, IWebDriver driver)
-        {
-            Console.WriteLine($"Rewards Automater complete. Program will end in {seconds} seconds...");
-            Thread.Sleep(1000 * seconds);
-            driver.Quit();
-        }
-    }
+	public void CloseSelenium(int seconds)
+	{
+		Console.WriteLine($"Rewards Automater complete. Program will end in {seconds} seconds...");
+		Thread.Sleep(1000 * seconds);
+		_driver.Quit();
+	}
 }
