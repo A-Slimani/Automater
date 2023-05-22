9168116e6a101f97d6e70060d6a3c9dd63fad0c5
A-Slimani <abdullahslimani98@gmail.com>
2023-05-22 17:39:08 +1000

    updates

--- Automater/Program.cs
+++ Automater/Program.cs
@@ -3,16 +3,23 @@ using Spectre.Console;
 
 class Program
 {
-	static void Main(string[] args)
-	{
-		var options = new EdgeOptions();
-		options.AddExcludedArgument("enable-logging");
-		var driver = new EdgeDriver(options);
-		var bingFunctions = new BingFunctions(driver);
-
-		// bingFunctions.AutomatedSearches();
-		bingFunctions.ActivateRewardCards();
-		bingFunctions.ActivateQuestAndPunchCards();
-		bingFunctions.CloseSelenium(15);
-	}
+  static void Main(string[] args)
+  {
+    var options = new EdgeOptions();
+    options.AddExcludedArgument("enable-logging");
+    var driver = new EdgeDriver(options);
+    var bingFunctions = new BingFunctions(driver);
+    try
+    {
+      // bingFunctions.AutomatedSearches();
+      bingFunctions.ActivateRewardCards();
+      bingFunctions.ActivateQuestAndPunchCards();
+      bingFunctions.CloseSelenium(15);
+    }
+    catch (Exception ex)
+    {
+      AnsiConsole.WriteException(ex);
+      bingFunctions.CloseSelenium(15);
+    }
+  }
 }
