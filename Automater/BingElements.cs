f2ac3d0603f0fb3a69eca9aa419f8530e446f3bf
A-Slimani <abdullahslimani98@gmail.com>
2023-07-14 14:36:39 +1000

    formatting updates

--- Automater/BingElements.cs
+++ Automater/BingElements.cs
@@ -4,77 +4,77 @@ using Spectre.Console;
 
 namespace Automater
 {
-    public static class BingElements
+  public static class BingElements
+  {
+    public static int GetPointsEarnedToday(IWebDriver driver)
     {
-        public static int GetPointsEarnedToday(IWebDriver driver)
-        {
-            driver.Navigate().GoToUrl("https://rewards.bing.com");
-            Thread.Sleep(1000);
-            var pointsEarnedToday = driver.FindElement(By.XPath("//*[@id=\"dailypointToolTipDiv\"]/p/mee-rewards-counter-animation/span"));
-            return int.Parse(pointsEarnedToday.Text);
-        }
-
-        public static int GetRemainingSearches(IWebDriver driver)
-        {
-            driver.Navigate().GoToUrl("https://rewards.bing.com");
-
-            var pointsBreakDownElement = driver.FindElement(By.Id("dailypointColumnCalltoAction"));
-            pointsBreakDownElement.Click();
-
-            var pcSearchElement = driver.FindElement(By.XPath("//p[@class=\"pointsDetail c-subheading-3 ng-binding\"]"));
-
-            int currentPoints = int.Parse(pcSearchElement.Text.Split('/')[0].Trim()); //  just gets the first number
-
-            int remainingSearches = (90 - currentPoints) / 3;
-
-            AnsiConsole.MarkupLine($"[Blue]{remainingSearches}[/] searches remaining...");
-
-            return remainingSearches;
-        }
-
-        public static IEnumerable<IWebElement> FilterElements(ICollection<IWebElement> webElements, Regex regex)
-        {
-            return webElements.Where(element => regex.IsMatch(element.GetAttribute("innerHTML")));
-        }
-
-        // for debugging purposes -> put at the bottom later
-        public static void DownloadAllElementsToFile(ICollection<IWebElement> elements, string filename, Func<IWebElement, string> getInfo, string fileType)
-        {
-            // this wont work on other devices
-            string filepath = @$"D:\programming\C#\Automater\Automater\{filename}.{fileType}";
-
-            if (File.Exists(filepath)) File.Delete(filepath);
-
-            string result = $"{elements.Count} results found for {filename}";
-            Console.WriteLine(result);
-
-            foreach (var element in elements)
-            {
-                if (!string.IsNullOrEmpty(getInfo(element)))
-                {
-                    File.AppendAllText(filepath, getInfo(element));
-                    File.AppendAllText(filepath, "\n");
-                }
-            }
-        }
-
-        public static IEnumerable<IWebElement> GetAllClickableElements(ICollection<IWebElement> webElements)
-        {
-            foreach (var element in webElements)
-            {
-                bool isClickable = false;
-
-                try
-                {
-                    isClickable = element.Enabled && element.Displayed;
-                }
-                catch (StaleElementReferenceException ex)
-                {
-                    Console.WriteLine($"Error {ex.Message}");
-                }
-
-                if (isClickable) yield return element;
-            }
-        }
+      driver.Navigate().GoToUrl("https://rewards.bing.com");
+      Thread.Sleep(1000);
+      var pointsEarnedToday = driver.FindElement(By.XPath("//*[@id=\"dailypointToolTipDiv\"]/p/mee-rewards-counter-animation/span"));
+      return int.Parse(pointsEarnedToday.Text);
     }
+
+    public static int GetRemainingSearches(IWebDriver driver)
+    {
+      driver.Navigate().GoToUrl("https://rewards.bing.com");
+
+      var pointsBreakDownElement = driver.FindElement(By.Id("dailypointColumnCalltoAction"));
+      pointsBreakDownElement.Click();
+
+      var pcSearchElement = driver.FindElement(By.XPath("//p[@class=\"pointsDetail c-subheading-3 ng-binding\"]"));
+
+      int currentPoints = int.Parse(pcSearchElement.Text.Split('/')[0].Trim()); //  just gets the first number
+
+      int remainingSearches = (90 - currentPoints) / 3;
+
+      AnsiConsole.MarkupLine($"[Blue]{remainingSearches}[/] searches remaining...");
+
+      return remainingSearches;
+    }
+
+    public static IEnumerable<IWebElement> FilterElements(ICollection<IWebElement> webElements, Regex regex)
+    {
+      return webElements.Where(element => regex.IsMatch(element.GetAttribute("innerHTML")));
+    }
+
+    // for debugging purposes -> put at the bottom later
+    public static void DownloadAllElementsToFile(ICollection<IWebElement> elements, string filename, Func<IWebElement, string> getInfo, string fileType)
+    {
+      // this wont work on other devices
+      string filepath = @$"D:\programming\C#\Automater\Automater\{filename}.{fileType}";
+
+      if (File.Exists(filepath)) File.Delete(filepath);
+
+      string result = $"{elements.Count} results found for {filename}";
+      Console.WriteLine(result);
+
+      foreach (var element in elements)
+      {
+        if (!string.IsNullOrEmpty(getInfo(element)))
+        {
+          File.AppendAllText(filepath, getInfo(element));
+          File.AppendAllText(filepath, "\n");
+        }
+      }
+    }
+
+    public static IEnumerable<IWebElement> GetAllClickableElements(ICollection<IWebElement> webElements)
+    {
+      foreach (var element in webElements)
+      {
+        bool isClickable = false;
+
+        try
+        {
+          isClickable = element.Enabled && element.Displayed;
+        }
+        catch (StaleElementReferenceException ex)
+        {
+          Console.WriteLine($"Error {ex.Message}");
+        }
+
+        if (isClickable) yield return element;
+      }
+    }
+  }
 }
--- Automater/BingHelperFunctions.cs
+++ Automater/BingHelperFunctions.cs
@@ -6,106 +6,106 @@ using Spectre.Console;
 
 namespace Automater
 {
-    public enum BingFunctionType
+  public enum BingFunctionType
+  {
+    RewardCard,
+    PunchCard
+  }
+
+  public static class BingHelperFunctions
+  {
+    public static void OpenSetOfElements(IWebElement element, WebDriverWait wait, BingFunctionType type)
     {
-        RewardCard,
-        PunchCard
+      wait.Until(driver =>
+      {
+        var scriptResult = ((IJavaScriptExecutor)driver).ExecuteScript("return document.readyState");
+        if (scriptResult.Equals("complete"))
+        {
+          if (type == BingFunctionType.RewardCard)
+          {
+            string elementText = element.Text.Split(new string[] { "\n" }, StringSplitOptions.None)[1];
+            driver.SwitchTo().Window(driver.WindowHandles.Last()).Close();
+            driver.SwitchTo().Window(driver.WindowHandles.First());
+            AnsiConsole.MarkupLine($"ELEMENT CARD: {elementText} [green]Complete[/]");
+            return true;
+          }
+          else
+          {
+            // I dont think I can get the element text like in the conditional above
+            // need a different way to get the text 
+            // also need to find quiz questions within the punch cards
+            element.Click();
+            driver.SwitchTo().Window(driver.WindowHandles.Last()).Close();
+            driver.SwitchTo().Window(driver.WindowHandles[1]);
+            return true;
+          }
+        }
+        else
+        {
+          AnsiConsole.MarkupLine($"{element.Text} [red]Failed[/]");
+          return false;
+        }
+      });
     }
 
-    public static class BingHelperFunctions
+    public static void AnswerQuestions(IWebDriver driver)
     {
-        public static void OpenSetOfElements(IWebElement element, WebDriverWait wait, BingFunctionType type)
+      // Phase 1: Click on the Start Quiz button if it exists
+      var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+      try
+      {
+        var startQuizElement = wait.Until(ExpectedConditions.ElementToBeClickable(By.Id("rqStartQuiz")));
+        driver.ExecuteJavaScript("arguments[0].click()", startQuizElement);
+      }
+      catch
+      {
+        AnsiConsole.MarkupLine("[red]Start Quiz button not found continuing the quiz... [/]");
+      }
+
+      // Phase 2: Check what class is being used 
+      IList<IWebElement> answerElements;
+      string[] answerElementsClasses = { "bt_cardText", "rqOption", "btOptionCard" };
+      string currentCSSTag = answerElementsClasses[0];
+      answerElements = driver.FindElements(By.ClassName(answerElementsClasses[0]));
+      int count = 1; // to satisfy the while loop expression 
+      while (answerElements.Count == 0)
+      {
+        answerElements = driver.FindElements(By.ClassName(answerElementsClasses[count]));
+        currentCSSTag = answerElementsClasses[count];
+        count++;
+      }
+
+      if (answerElements.Count != 1)
+      {
+        // Phase 3: Click on all answers
+        try
         {
-            wait.Until(driver =>
-            {
-                var scriptResult = ((IJavaScriptExecutor)driver).ExecuteScript("return document.readyState");
-                if (scriptResult.Equals("complete"))
-                {
-                    if (type == BingFunctionType.RewardCard)
-                    {
-                        string elementText = element.Text.Split(new string[] { "\n" }, StringSplitOptions.None)[1];
-                        driver.SwitchTo().Window(driver.WindowHandles.Last()).Close();
-                        driver.SwitchTo().Window(driver.WindowHandles.First());
-                        AnsiConsole.MarkupLine($"ELEMENT CARD: {elementText} [green]Complete[/]");
-                        return true;
-                    }
-                    else
-                    {
-                        // I dont think I can get the element text like in the conditional above
-                        // need a different way to get the text 
-                        // also need to find quiz questions within the punch cards
-                        element.Click();
-                        driver.SwitchTo().Window(driver.WindowHandles.Last()).Close();
-                        driver.SwitchTo().Window(driver.WindowHandles[1]);
-                        return true;
-                    }
-                }
-                else
-                {
-                    AnsiConsole.MarkupLine($"{element.Text} [red]Failed[/]");
-                    return false;
-                }
-            });
-        }
+          int totalPoints = int.Parse(driver.FindElement(By.ClassName("rqMCredits")).Text) / 10;
 
-        public static void AnswerQuestions(IWebDriver driver)
+          if (totalPoints == 0)
+          {
+            IList<string> thisOrThatElement = driver.FindElement(By.ClassName("bt_Quefooter")).Text.Split(' ').ToList();
+            totalPoints = int.Parse(thisOrThatElement[2]);
+          }
+
+          for (int i = 0; i < totalPoints; i++)
+          {
+            for (int j = 0; j < answerElements.Count; j++)
+            {
+              answerElements = driver.FindElements(By.ClassName(currentCSSTag));
+              driver.ExecuteJavaScript("arguments[0].click()", answerElements[j]);
+            }
+          }
+        }
+        catch
         {
-            // Phase 1: Click on the Start Quiz button if it exists
-            var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
-            try
-            {
-                var startQuizElement = wait.Until(ExpectedConditions.ElementToBeClickable(By.Id("rqStartQuiz")));
-                driver.ExecuteJavaScript("arguments[0].click()", startQuizElement);
-            }
-            catch
-            {
-                AnsiConsole.MarkupLine("[red]Start Quiz button not found continuing the quiz... [/]");
-            }
-
-            // Phase 2: Check what class is being used 
-            IList<IWebElement> answerElements;
-            string[] answerElementsClasses = { "bt_cardText", "rqOption", "btOptionCard" };
-            string currentCSSTag = answerElementsClasses[0];
-            answerElements = driver.FindElements(By.ClassName(answerElementsClasses[0]));
-            int count = 1; // to satisfy the while loop expression 
-            while (answerElements.Count == 0)
-            {
-                answerElements = driver.FindElements(By.ClassName(answerElementsClasses[count]));
-                currentCSSTag = answerElementsClasses[count];
-                count++;
-            }
-
-            if (answerElements.Count != 1)
-            {
-                // Phase 3: Click on all answers
-                try
-                {
-                    int totalPoints = int.Parse(driver.FindElement(By.ClassName("rqMCredits")).Text) / 10;
-
-                    if (totalPoints == 0)
-                    {
-                        IList<string> thisOrThatElement = driver.FindElement(By.ClassName("bt_Quefooter")).Text.Split(' ').ToList();
-                        totalPoints = int.Parse(thisOrThatElement[2]);
-                    }
-
-                    for (int i = 0; i < totalPoints; i++)
-                    {
-                        for (int j = 0; j < answerElements.Count; j++)
-                        {
-                            answerElements = driver.FindElements(By.ClassName(currentCSSTag));
-                            driver.ExecuteJavaScript("arguments[0].click()", answerElements[j]);
-                        }
-                    }
-                }
-                catch
-                {
-                    AnsiConsole.MarkupLine("[yellow]Quiz Complete...[/]");
-                }
-            }
-            else
-            {
-                AnsiConsole.MarkupLine("[yellow]Quiz Complete...[/]");
-            }
+          AnsiConsole.MarkupLine("[yellow]Quiz Complete...[/]");
         }
+      }
+      else
+      {
+        AnsiConsole.MarkupLine("[yellow]Quiz Complete...[/]");
+      }
     }
+  }
 }
