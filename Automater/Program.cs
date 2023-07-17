e25a0599b50bb114dd3572bc5bb9b93129a35d1a
A-Slimani <abdullahslimani98@gmail.com>
2023-07-17 16:58:52 +1000

    fixes

--- Automater/Program.cs
+++ Automater/Program.cs
@@ -61,6 +61,7 @@ class Program
       Log.Error(ex.ToString());
     }
 
+    Log.Information($"POINTS EARNED TODAY: {BingElements.GetPointsEarnedToday(mobileDriver)}");
 
     /*
 		driver.Navigate().GoToUrl("https://www.bing.com/search?q=test&form=QBLH&sp=-1&lq=0&pq=tes&sc=10-3&qs=n&sk=&cvid=76F1B249122840479379B9E2F249D038&ghsh=0&ghacc=0&ghpl=");
